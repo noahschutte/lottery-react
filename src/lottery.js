ada87d823e9d1f9f69e6c1df1c480979899ac4db
Noah Schutte <noahschutte@noahs-mbp.lan>
2020-08-28 15:37:33 -0600

    0x2C5f6d64Cf57514A6b44F8155F31C5A340637943

--- src/lottery.js
+++ src/lottery.js
@@ -1,58 +1,58 @@
-import web3 from "./web3";
+import web3 from './web3';
 
-const address = "0x2C5f6d64Cf57514A6b44F8155F31C5A340637943";
+const address = '0x2C5f6d64Cf57514A6b44F8155F31C5A340637943';
 const abi = [
   {
     constant: true,
     inputs: [],
-    name: "manager",
-    outputs: [{ name: "", type: "address" }],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
     payable: false,
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function'
   },
   {
     constant: false,
     inputs: [],
-    name: "pickWinner",
+    name: 'pickWinner',
     outputs: [],
     payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
     constant: true,
     inputs: [],
-    name: "getPlayers",
-    outputs: [{ name: "", type: "address[]" }],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
     payable: false,
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function'
   },
   {
     constant: false,
     inputs: [],
-    name: "enter",
+    name: 'enter',
     outputs: [],
     payable: true,
-    stateMutability: "payable",
-    type: "function",
+    stateMutability: 'payable',
+    type: 'function'
   },
   {
     constant: true,
-    inputs: [{ name: "", type: "uint256" }],
-    name: "players",
-    outputs: [{ name: "", type: "address" }],
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
     payable: false,
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function'
   },
   {
     inputs: [],
     payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  }
 ];
 
 export default new web3.eth.Contract(abi, address);
